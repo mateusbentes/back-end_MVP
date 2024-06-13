1e45e993859e31679ae429df1386d9e2d7ad3fc9
mateusbentes <mateusbentes@gmail.com>
2024-06-13 14:33:39 -0300

    comentada nota especifica

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ class Nota(db.Model):
         """Função que obtem todas as notas no banco de dados"""
         return [Nota.json(nota) for nota in Nota.query.all()] # Retorna a busca te todas as notas
     
-    def obter_nota(id):
-        """Funcão para obter a nota usando o id da nota como parametro"""
-        Nota.json(Nota.query.filter_by(id=id).first().json()) # Procura a a nota no banco de dados pelo id como json
+    #def obter_nota(id):
+        #"""Funcão para obter a nota usando o id da nota como parametro"""
+        #Nota.json(Nota.query.filter_by(id=id).first().json()) # Procura a a nota no banco de dados pelo id como json
 
     def adicao_nota(titulo, texto):
         """Função para adicionar nota no banco de dados usando titulo e texto como parametros"""
@@ -66,7 +66,7 @@ class Nota(db.Model):
 # definindo as tags
 api_tag = Tag(name="Documentação", description="Seleção de documentação: Swagger, Redoc ou RapiDoc")
 tudo_tag = Tag(name="Obter notas", description="Obter todas as nota")
-especifico_tag = Tag(name="Obter nota especifica", description="Obter nota especifica")
+#especifico_tag = Tag(name="Obter nota especifica", description="Obter nota especifica")
 adicao_tag = Tag(name="Adicao de nota", description="Adicao de nota no bloco")
 atualizacao_tag = Tag(name="Atualizacao de nota", description="Atualizacao de nota no bloco")
 deletar_tag = Tag(name="Deletar nota", description="Apagar de nota no bloco")
@@ -81,12 +81,12 @@ def obter_notas():
     """Função que obtem todas as notas no banco de dados"""
     return jsonify({'Notas' : Nota.obter_todas_notas()})
 
-@app.get('/', methods=['GET'] , tags=[especifico_tag])
-def obter_nota_por_id():
-    """Função que obtem nota específica no banco de dados"""
-    requisicao_de_dados = request.get_json() # Obtem o dado do cliente
-    id = requisicao_de_dados.get('id')
-    return jsonify(Nota.obter_nota(id))
+#@app.get('/', methods=['GET'] , tags=[especifico_tag])
+#def obter_nota_por_id():
+    #"""Função que obtem nota específica no banco de dados"""
+    #requisicao_de_dados = request.get_json() # Obtem o dado do cliente
+    #id = requisicao_de_dados.get('id')
+    #return jsonify(Nota.obter_nota(id))
 
 @app.post('/', methods=['POST'] , tags=[adicao_tag])
 def adicao_nota():
